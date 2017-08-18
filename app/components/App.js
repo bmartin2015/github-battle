e3926bd01e032491ab0c41f8af7d02f5c941491b
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-08-18 14:52:01 -0500

    Add Nav and basic battle

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Popular from './Popular';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route
+              render={() => {
+                return <p>Quoth the Raven, 404</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
