57cff8f504a14fc3ca657ca77102eaa7a115793d
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-08-18 10:34:12 -0500

    Add popular component and setup languages

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Popular from './Popular';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
   }
 }
 
